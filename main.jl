7587c36b5d99abadb80c762841a9e6badbd20e74
Greg Neustroev <G.Neustroev@tudelft.nl>
2024-05-01 18:23:03 +0200

    Add sample script

---
+++ main.jl
@@ -0,0 +1,19 @@
+using GenerationExpansionPlanning
+using Gurobi
+
+# Step 1: Read the experiment config
+@info "Reading the config"
+config_path = "case_studies/stylized_EU/config.toml"
+config = read_config(config_path)
+
+# Step 2: Parse the data
+@info "Parsing the config data"
+experiment_data = ExperimentData(config[:input])
+
+# Step 3: Run the experiments
+@info "Running the experiments defined by $config_path"
+experiment_result = run_experiment(experiment_data, Gurobi.Optimizer)
+
+# Step 4: Save the results
+output_config = config[:output]
+save_result(experiment_result, output_config)
